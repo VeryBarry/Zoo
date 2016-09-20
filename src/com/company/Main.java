20e988c470392828c6039b6d9d7e2bd99212d94b
VeryBarry <barry.a.daniels@gmail.com>
2016-09-20 12:06:39 -0400

    @stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +16,28 @@ public class Main {
         System.out.println(c);
         System.out.println(f);
         System.out.println(s);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type an animal name:");
+        String name = scanner.nextLine();
+
+        Animal a = createAnimal(name);
+        System.out.println(a);
+    }
+    static Animal createAnimal(String name) {
+        Animal animal;
+        if (name.equalsIgnoreCase("dog")){
+            animal = new Cougar();
+        }
+        else if (name.equalsIgnoreCase("hawk")){
+            animal = new Falcon();
+        }
+        else if (name.equalsIgnoreCase("snake")){
+            animal = new Snake();
+        }
+        else {
+            animal = new Animal();
+        }
+        return animal;
     }
 }
